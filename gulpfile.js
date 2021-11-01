f5de4e33aba734e4d09e501aeb6bb4c9719b9077
pankowskitomasz <pankowski_tomasz@o2.pl>
2021-11-01 12:19:56 +0100

    layout update

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const {src,dest,parallel} = require("gulp");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const minify = require("gulp-minify-css")
+
+function js(){
+    return src([
+        "node_modules/jquery/dist/jquery.min.js",
+        "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+        "js/main.js"
+    ])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("js/"));
+}
+
+function css(){
+    return src([
+        "node_modules/bootstrap/dist/css/bootstrap.min.css",
+        "css/styles.css",
+        "node_modules/font-awesome/css/font-awesome.min.css"
+    ])
+    .pipe(concat("styles.min.css"))
+    .pipe(minify())
+    .pipe(dest("css/"));
+}
+
+exports.js = js;
+exports.css = css;
+exports.default = parallel([js,css]);
